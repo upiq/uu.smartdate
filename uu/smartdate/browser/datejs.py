3f3bee0e925fdfeb07792a711b2feb417933e808
Sean Upton <sdupton@gmail.com>
2011-08-03 14:00:31 -0600

    datejs redirector needed a bit of rewrite around a bug in Plone integration of Zope2 requests: Products.PlacelessTranslationServices.Negotiator.PTSLanguages.getPreferredLanguages() does the wrong thing, which means self.request.locale never gets the proper territory.  Use zope.publisher.browser.BrowserLanguages and zope.i18n to explicitly construct our own locale object instead of relying on request.locale

--- uu/smartdate/browser/datejs.py
+++ uu/smartdate/browser/datejs.py
@@ -1,5 +1,10 @@
 import os
 
+from zope.component import getMultiAdapter
+from zope.i18n.locales import locales, LoadLocaleError
+from zope.publisher.browser import BrowserLanguages
+
+
 # some initialized globals for use by all view instances
 PRODNAME = 'uu.smartdate'
 BASE_PATH = os.path.dirname(__file__)
@@ -17,15 +22,29 @@ class DateJSRedirectView(object):
         self.locale_id = self.request.locale.id
     
     def __call__(self, *args, **kwargs):
-        lang = self.locale_id.language.lower()
-        terr = self.locale_id.territory.upper()
-        script = self.locale_id.script
-        if script is not None:
-            lang = '%s-%s' % (lang, script.title())
-        filename = 'date-%s-%s.js' % (lang, terr)
-        if filename not in DATEJS_FILES:
-            filename = DEFAULT_FILENAME # always redirect to file that exists
-        resource = '++resource++%s-datejs/%s' (PRODNAME, filename)
+        site_url = getMultiAdapter(
+            (self.context, self.request),
+            name=u'plone_portal_state').portal_url()
+        locale = None
+        languages = BrowserLanguages(self.request).getPreferredLanguages()
+        for lang in languages:
+            parts = (lang.split('-') + [None, None])[:3]
+            try:
+                locale = locales.getLocale(*parts)
+                break
+            except LoadLocaleError:
+                 pass
+        filename = DEFAULT_FILENAME
+        if locale is not None and locale.id.territory is not None:
+            lang = locale.id.language.lower()
+            terr = locale.id.territory.upper()
+            script = self.locale_id.script
+            if script is not None:
+                lang = '%s-%s' % (lang, script.title())
+            filename = 'date-%s-%s.js' % (lang, terr)
+            if filename not in DATEJS_FILES:
+                filename = DEFAULT_FILENAME # always redirect to file that exists
+        resource = '++resource++%s-datejs/%s' % (PRODNAME, filename)
         url = '/'.join((site_url, resource))
         self.request.response.redirect(url, status=302)
 
