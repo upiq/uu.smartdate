a60cc3996796d96f0ef05fceb9ff7fb7bfbc467a
Sean Upton <sdupton@gmail.com>
2011-08-03 16:49:22 -0600

    changed calendar icon to tango icon (Public domain); changed jsregistry to omit JQT plugins (unneeded); removed extraneous banner comments from smartdate.js

--- uu/smartdate/browser/resources/scripts/smartdate.js
+++ uu/smartdate/browser/resources/scripts/smartdate.js
@@ -1,81 +1,8 @@
 /**
-*   smartdate.js -  a smart date form widget atop a simple HTML5 date input
-*                   or an (X)HTML text input.
+*   smartdate.js / seanupton@hsc.utah.edu
 * 
-*   A keyboard-friendly mashup of Date.js (http://datejs.com) and the
-*   JQuery Tools Dateinput picker (http://flowplayer.org/tools/dateinput/).
-*
-*   Features and end-user operation
-*   -------------------------------
-*
-*     Calendar picker (from JQuery Tools)
-*
-*     Natural-language date parsing helpful for common shortcuts (date.js).
-*
-*     HTML5 placeholder text for browsers that support it (every modern
-*     browser excepting MSIE), and tooltip hints for all browsers
-*     (including IE).
-*     
-*     Basic format normalization (at the moment, to USA-style MM/DD/YYYY).
-*
-*     Ability to support future/historical dates via keyboard input, even
-*     if they are beyond the default range of the JQuery Tools date picker
-*     (which defaults to +/- 5 years).
-*     
-*     Keyboard-friendly:
-*
-*       * Normal HTML input boxes with normal tab in/out behavior.
-*
-*       * Typing partial dates will yield suggestions in an overlay.
-*
-*       * Pressing <ENTER> or <TAB> will accept any suggestion.
-*
-*           * <ENTER> remains in the field.
-*
-*           * <TAB> leaves the field and moves the focus to the next
-*             widget in the form.
-*
-*       * Pressing the DOWN arrow button shows the calendar picker.
-* 
-*       * Pressing <ESC> will clear any date entered, and hides any 
-*         exposed calendar picker.
-*
-*   Usage notes / requirements
-*   --------------------------
-*
-*   HTML requirements (see demo.html in included distribution):
-* 
-*       * HTML includes date.js (http://datejs.com)
-*
-*       * HTML includes JQuery 1.4.x (http://jquery.com)
-*
-*       * HTML includes JQuery Tools 1.2.x (http://flowplayer.org/tools)
-*
-*       * HTML includes date.css and next/previous button images (via Plone)
-*
-*       * HTML wraps inputs, labels in <div class="smartdate">
-*           (this is more for styling than for core functionality)
-*
-*       * Recommended:  HTML uses CSS image-replacement for calendar triggeri
-*                       hyperlink created in DOM (by javascript).
-*
-*   Scripting requirements:
-* 
-*       * After new calendar inputs are added to the dom, run the function
-*         smartdate.hookups().  This will (re-)hookup all event handlers
-*         and DOM modifications for existing and new widgets added to the
-*         page.
-*
-*   ----
-*
-*   Author: Sean Upton <sean.upton@hsc.utah.edu> [2011-07-27]
-*
-*   Copying / License:
-*
-*       This file and demo.html file are MIT-style (see COPYING.txt).
-*
-*       Other included files possibly bundled with any distribution are
-*       licensed under respective upstream open-source licenses.
+*   (c) 2011 The University of Utah -- this JavaScript source available as
+*       free software under either GNU GPLv2 and MIT-style at your choosing.
 *
 */
 
