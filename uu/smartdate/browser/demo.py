68618ebb84a9ec22c29b38f9a8b7a55774b2717a
Sean Upton <sdupton@gmail.com>
2011-08-04 17:15:46 -0600

    display form template for widget (hooked up, copied from z3c.form.browser text_display.pt).

--- uu/smartdate/browser/demo.py
+++ uu/smartdate/browser/demo.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from plone.z3cform.layout import wrap_form
 from z3c.form import form, field, button
 from zope.interface import Interface
@@ -11,6 +13,14 @@ class IDateDemoSchema(Interface):
     title = schema.TextLine(title=u'title', required=False)
     start = schema.Date(title=u'Start date', required=True)
     stop = schema.Date(title=u'Stop date', required=True)
+
+class DateDemoRecord(object):
+    """Dummy record for demo"""
+    implements(IDateDemoSchema)
+    def __init__(self, title=u'', start=date.today(), end=date.today()):
+        self.title = title
+        self.start = start
+        self.end = end
 
 
 class DateDemoForm(form.Form):
@@ -38,7 +48,26 @@ class DateDemoForm(form.Form):
             print data['stop']
 
 
+class DateDemoDisplayForm(form.DisplayForm):
+    """Display form for date demo"""
+    fields = field.Fields(IDateDemoSchema)
+    ignoreContext = True
+    label = u'Date form demo'
+    enctype = 'multipart/form-data'
+
+    def updateWidgets(self):
+        datefields = [f.field for f in self.fields.values()
+                        if schema.interfaces.IDate.providedBy(f.field)]
+        for field in datefields:
+            self.fields[field.__name__].widgetFactory = SmartdateFieldWidget
+        super(DateDemoDisplayForm, self).updateWidgets()
+ 
+
+
 DateDemoFormView = wrap_form(DateDemoForm)
+DateDemoDisplayFormView = wrap_form(DateDemoDisplayForm)
+
+
 
 class SmartDateDemoView(object):
     """Simple dummy view for testing a static version of the form"""
